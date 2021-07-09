d1a4afc81aec7c8e4cf692f9cb6bf6dc706bec10
ssumniee <suemink.dev@gmail.com>
2021-07-09 12:10:02 +0900

    route practice first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <div id="buttonContainer">
+            <Link to="/" className="linkButton">
+              Home
+            </Link>
+
+            <Link to="/about" className="linkButton">
+              Mypage
+            </Link>
+
+            <Link to="/dashboard" className="linkButton">
+              Dashboard
+            </Link>
+          </div>
+        </nav>
+      </div>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <Mypage />
+        </Route>
+        <Route exact path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
+
+const Home = () => {
+  return <h1>Home</h1>;
+};
+
+const Mypage = () => {
+  return <h1>Mypage</h1>;
+};
+
+const Dashboard = () => {
+  return <h1>Dashboard</h1>;
+};
 
 export default App;
